9238e2dbf7afec0ddd9b5d132a9c75050f623447
Karla Dayhoff <kddayhoff@gmail.com>
2020-04-29 21:18:19 -0500

    updated info in .env and server.js accordingly. added dotenv package

--- db/server.js
+++ db/server.js
@@ -2,6 +2,7 @@ const employeeCMS = require("../employee-CMS.js")
 const mysql = require("mysql");
 const express = require("express");
 const app = express();
+const env = require("../.env");
 
 const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
@@ -10,18 +11,13 @@ app.use(express.static('public'));
 
 // Sets up the Express app to handle data parsing
 var connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
+  host: env.DB_HOST,
   port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "Weezer00!",
+  username: env.DB_USER,
+  password: env.DB_PASS,
   database: "employee_cmsDB"
 });
+
 
 connection.connect(function (err) {
     console.log("SQL connected as id " + connection.threadId)
